558ce884e83848297d8f15e317fb655f1ad8a97a
ndxbxrme <lewis_the_cat@hotmail.com>
2017-03-01 14:57:02 +0000

    dogit

--- dogit.bat
+++ dogit.bat
@@ -2,6 +2,6 @@ call grunt build
 git add --all
 git commit -m %2
 git push origin master
-call npm version %1 --no-git-tag-version
 call bower version %1
+call npm version %1 --no-git-tag-version
 git push origin --tags
